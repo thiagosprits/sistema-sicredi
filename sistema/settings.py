0c8326448acdbc3dd9c88aed8ee62a4a486895e3
Seu Nome <seu.email@exemplo.com>
2025-01-01 21:14:21 -0400

    adiciona suporte ao postgres

--- sistema/settings.py
+++ sistema/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,10 +56,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sistema.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
