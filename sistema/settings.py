901ddd359d9b5e15a558f54bf8eea9124acd1b40
Seu Nome <seu.email@exemplo.com>
2025-01-01 21:41:02 -0400

    corrige settings.py

--- sistema/settings.py
+++ sistema/settings.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 import dj_database_url
-from django.contrib.auth.models import User
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
