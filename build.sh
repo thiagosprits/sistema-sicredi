6711bce8641a67cf1d7e005eb7ea75e5cfec0bd3
Seu Nome <seu.email@exemplo.com>
2025-01-01 21:47:57 -0400

    corrige configurações e remove referências ao User

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+# Upgrade pip
 python -m pip install --upgrade pip
+
+# Install requirements
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Apply migrations
 python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'sicredi2024') if not User.objects.filter(username='admin').exists() else None" | python manage.py shell 
+
+# Create superuser
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'sicredi2024') if not User.objects.filter(username='admin').exists() else None" 
